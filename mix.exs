5b42acad708fa347b228875d9df5560d5e7f62e5
Chris <Charlie.Mike.1991@gmail.com>
2020-08-20 19:14:16 -0400

    Error3 initial commit

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule TrainingSrvr.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phx_gen_auth, "~> 0.4"}
     ]
   end
 
